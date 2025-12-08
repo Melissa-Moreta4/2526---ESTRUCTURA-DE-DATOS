dc0c0f89a9e7705052e8de4ecb9587796b9f1362
Melissa-Moreta4 <mb.moretap@uea.edu.ec>
2025-12-07 21:33:06 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("como estan amiguitos");
+Console.WriteLine("como estan amiguitos, soy Melissa y cree este proyecto");
